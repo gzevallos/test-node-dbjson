98ed3102477830d89d7f9d81053b8dad8202a75f
gzevallos <gzevallos@digitalhouse.com>
2020-01-22 19:42:50 -0300

    cambio boludo

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.listen(3030, () => console.log('Server is running...'));
+app.get('/', (req, res) => {
+   res.send('Hello world!');
+});
+
+app.listen(3000, () => console.log('Server is running...'));
